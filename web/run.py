daf26cd0fc3c2720235bc92ea1da9a719b63834b
hamedabo <hamedsallam96@gmail.com>
2022-01-03 23:26:26 +0200

    add FlaskMarket project

---
+++ web/run.py
@@ -0,0 +1,7 @@
+import os
+from market import app
+# Checks if the run.py file has executed directly and not imported
+# Run this command to run the application in DEBUG mode
+# docker run -p 5000:5000 -e DEBUG=1 <image-name>
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=os.environ.get('DEBUG') == '1')
